54abbd2caf82ffee5947837fbd4e8e4ae4c43879
Jakasco <asddsa@dsa.com>
2019-03-24 18:09:55 +0200

    'viesti'

--- src/components/PicArray.js
+++ src/components/PicArray.js
@@ -1,19 +1,20 @@
 import React, {Component} from 'react';
 
+const url = 'http://media.mw.metropolia.fi/wbma/uploads/';
+
 class PicArray extends Component {
     render() {
         return this.props.picArray.map((picArr, i) => (
-
             <tr key={i}>
                 <td>
-                    <img src={picArr.thumbnail} alt={picArr.title} />
+                    <img src={url + picArr.thumbnails.w160} alt={picArr.title} />
                 </td>
                 <td>
-                    <h3>{picArr.title}</h3>
-                    <p>{picArr.details}</p>
+                    <h4>{picArr.title}</h4>
+                    <p>{picArr.description}</p>
                 </td>
                 <td>
-                    <a href={picArr.filename}>View</a>
+                    <a href={url + picArr.filename}>View</a>
                 </td>
             </tr>
 
