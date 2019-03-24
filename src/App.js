e47c075f984f75f0da34280deddff992606eef84
jakasco <jakasco@users.noreply.github.com>
2019-03-24 18:16:38 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -2,30 +2,42 @@ import React, {Component} from 'react';
 import './App.css';
 import PicArray from './components/PicArray';
 
+const url = 'http://media.mw.metropolia.fi/wbma/media/';
+
 class App extends Component {
 
     state = {
         picArray: [ ],
     };
 
-    componentDidMount() {
-        fetch( 'test.json').then((response) => {
-            return response.json();
-        }).then( (json) => {
-            console.log("Json:",json);
-            this.setState({picArray:json});
-        })
-    }
-
-    render() {
-        return (
-            <table>
-                <tbody>
-                <PicArray picArray={this.state.picArray}/>
-                </tbody>
-            </table>
-        );
-    }
+componentDidMount() {
+    fetch( url).then((response) => {
+        return response.json();
+    }).then( (json) => {
+        json.map(item => {
+            return fetch(url + item.file_id).then(response => {
+                return response.json();
+            }).then(items => {
+                console.log(items);
+                this.setState({
+                    picArray: [...this.state.picArray, items]
+                })
+            });
+        });
+    });
 }
 
+
+
+
+render() {
+    return (
+        <table>
+            <tbody>
+            <PicArray picArray={this.state.picArray}/>
+            </tbody>
+        </table>
+    );
+    }
+}
 export default App;
